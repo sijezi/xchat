d5646c5e817b414ba997b4c99cb494a341372cd3
sijezi <mawehelie@gmail.com>
2017-03-23 22:38:18 -0700

    loaded all libs

--- app.js
+++ app.js
@@ -7,12 +7,18 @@ var PORT = process.env.PORT || 3000;
 // Import the express framework
 var express = require('express');
 var app = express();
-
 // Import the http built-in moodule and save it to the http varaible
 var http = require('http').Server(app);
 
+// passing http.Server instance
+var socket = require('socket.io')(http);
+
 app.use(express.static(__dirname + '/public'));
 
+// listen for events
+socket.on('connection', function() {
+  console.log('User connected via socket.io');
+})
 // start the Server
 http.listen(PORT, function() {
     console.log('Server running');
